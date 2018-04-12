a07c35a39a3a3cc4ec82e12bb30777efc4d80f56
Unknown <piotr.welpa@gmail.com>
2018-04-12 18:12:01 +0200

    Model implementation

--- app/src/main/java/com/example/piotr/bakingapp/ui/MainActivity.java
+++ app/src/main/java/com/example/piotr/bakingapp/ui/MainActivity.java
@@ -1,7 +1,9 @@
-package com.example.piotr.bakingapp;
+package com.example.piotr.bakingapp.ui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.example.piotr.bakingapp.R;
 
 public class MainActivity extends AppCompatActivity {
 
