2d944265ce9c589fd0487e4140198853509bfcee
Unknown <piotr.welpa@gmail.com>
2018-04-12 18:51:57 +0200

    Create utils for downloading and parsing JSON data

--- app/src/main/java/com/example/piotr/bakingapp/ui/MainActivity.java
+++ app/src/main/java/com/example/piotr/bakingapp/ui/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.example.piotr.bakingapp.R;
+import com.example.piotr.bakingapp.utils.CakeController;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,5 +12,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        CakeController cakeController = new CakeController();
+        cakeController.start();
     }
 }
